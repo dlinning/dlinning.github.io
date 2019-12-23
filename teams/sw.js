0202a09a7f885acc30670cec1b02551650e10a0d
dlinning <douglinning@gmail.com>
2019-12-23 15:12:08 -0600

    Major rewrite

--- teams/sw.js
+++ teams/sw.js
@@ -1,20 +1,18 @@
-self.addEventListener("install", function(e) {
+self.addEventListener("install", function (e) {
 	e.waitUntil(
-		caches.open("dl-teams").then(function(cache) {
-			return cache.addAll(["./", "./index.html", "./style.css"]);
+		caches.open("dl-teams").then(function (cache) {
+			return cache.addAll(["./", "./index.html", "./style.css", "./script.js"]);
 		})
 	);
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", function (event) {
 	console.log("Activated sw.js", event);
 });
 
-self.addEventListener("fetch", function(event) {
-	console.log(event.request.url);
-
+self.addEventListener("fetch", function (event) {
 	event.respondWith(
-		caches.match(event.request).then(function(response) {
+		caches.match(event.request).then(function (response) {
 			return response || fetch(event.request);
 		})
 	);
